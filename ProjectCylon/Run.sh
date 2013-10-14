6e1b9cc18b4b6761645618286ea10bcff983be43
lek <leesupansa@gmail.com>
2013-10-14 15:18:28 +0700

    add ./Run.sh <option> = 1 not generate object before run

--- ProjectCylon/Run.sh
+++ ProjectCylon/Run.sh
@@ -4,8 +4,22 @@
 # Author: Lek
 # Created Date: 2013-10-09
 
+echo "----------------[Run.sh]-------------------"
+echo " USAGE $./Run.sh <option>          "
+echo " DEFAULT:: Automatic Generate object before run"
+echo " <Option> = 1:: NOT generate object before run"
+echo "-------------------------------------------"
+
+OPTION="$1"
+
+if [ "$OPTION" = 1 ] ; then 
 cd pageobjectdefinition
 ./GenAllPageObject.sh
 cd ../
+fi
+
+#echo "run sth..."
 behave --logging-level INFO --color --no-source --no-skipped
+
+
 
